fe39bc782850e8e67c19a0ce3c796a5e7188c882
hyeongyuc <hyeongyuc96@hutom.co.kr>
2021-04-02 17:38:53 +0900

    add train.sh

---
+++ train.sh
@@ -0,0 +1,11 @@
+for lr in 1e-2 1e-3 1e-4:
+    do
+        python train_camIO.py \
+        --project_name="robot_oob_0402" \
+        --max_epoch=3 \
+        --log_path="/OOB_RECOG/logs" \
+        --batch_size=32 \
+        --init_lr=${lr}
+    done
+done
+
