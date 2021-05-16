763816509071a8990e4fb5843f05ef1c31968e1b
Hyeongyuc96Hutom <hyeongyuc96@hutom.co.kr>
2021-05-17 01:34:53 +0900

    add section oob visuazliation

--- frame_visualization.py
+++ frame_visualization.py
@@ -9,6 +9,9 @@ import argparse
 import json
 import os
 
+import math
+import copy
+
 
 parser = argparse.ArgumentParser()
 
@@ -107,10 +110,17 @@ def apply_filter(assets, filter_type:str, kernel_size) : # input = numpy, kernel
 # for text on bar
 def present_text(ax, bar, text):
 	for rect in bar:
-		posx = rect.get_width()
+		posx = rect.get_x()
 		posy = rect.get_y() - rect.get_height()*0.3
 		print(posx, posy)
 		ax.text(posx, posy, text, rotation=0, ha='left', va='bottom')
+
+def present_text_for_section(ax, bar, pos_x, text):
+	for rect in bar:
+		posx = pos_x
+		posy = rect.get_y() + rect.get_height()*1.3
+		print(posx, posy)
+		ax.text(posx, posy, text, rotation=0, ha='left', va='top')
 
 # calc OOB_Metric
 # input|DataFrame = 'GT' 'model A' model B' ..
@@ -185,13 +195,14 @@ def main():
 	print(args.GT_path)
 	print('------')
 	GT_df = pd.read_csv(args.GT_path)
-	frame_label = list(GT_df['frame']) # sync xticks label from GT
-	time_label = list(GT_df['time']) # sync xticks label from GT
+	frame_label = list(GT_df['consensus_frame']) # sync xticks label from GT // video별 : frame , patient 별 : consensus_frame
+	time_label = list(GT_df['consensus_time']) # sync xticks label from GT // video별 : time , patient 별 : consensus_time
 
 	yticks = ['GT'] # y축 names # 순서중요
 	yticks += args.model_name
 
-	predict_data = {'GT': GT_df['truth']}
+	predict_data = {'GT': GT_df['truth']} #### origin
+	section_oob_dict = {}
 	
 	# pairwise read
 	for y_name, inf_path in zip(args.model_name, args.model_infernce_path) :
@@ -209,13 +220,16 @@ def main():
 	print(predict_data)
 	
 	'''
-	data = {'GT': [1, 1, 1, 0, 0, 1, 1, 1], # 0번 ~ 2번 frame , 5번 ~ 7번 frame
+	predict_data = {'GT': [1, 1, 1, 0, 0, 1, 1, 1], # 0번 ~ 2번 frame , 5번 ~ 7번 frame
 	        'Model A': [0, 0, 1, 1, 0, 0, 1, 1], # 2번 ~ 3번 frame
 			'Model B': [1, 0, 1, 1, 1, 0, 1, 0] # 0번 ~ 0번 frame, 2번 ~ 4번 frame, 6번 ~ 6번 frame
 			}
 	'''
 
-	## Data preprocessing
+	## find High OOB False Section
+	
+
+	#### Data Processing
 
 	# run-length encoding
 	encode_data = {}
@@ -257,11 +271,12 @@ def main():
 	print(runlength_model)
 
 	#### 2. matplotlib의 figure 및 axis 설정
-	fig, ax = plt.subplots(1,1,figsize=(12,8)) # 1x1 figure matrix 생성, 가로(7인치)x세로(5인치) 크기지정
+	fig, ax = plt.subplots(2,1,figsize=(18,13)) # 1x1 figure matrix 생성, 가로(7인치)x세로(5인치) 크기지정
+	print(fig)
 	
 	##### initalize label for legned, this code should be write before writing barchart #####
-	init_bar = ax.barh(range(len(yticks)), np.zeros(len(yticks)), label=label_names[IB], height=height, color=colors[IB]) # dummy data
-	init_bar = ax.barh(range(len(yticks)), np.zeros(len(yticks)), label=label_names[OOB], height=height, color=colors[OOB]) # dummy data
+	init_bar = ax[0].barh(range(len(yticks)), np.zeros(len(yticks)), label=label_names[IB], height=height, color=colors[IB]) # dummy data
+	init_bar = ax[0].barh(range(len(yticks)), np.zeros(len(yticks)), label=label_names[OOB], height=height, color=colors[OOB]) # dummy data
 	##### #### #### #### ##### #### #### #### 
 
 
@@ -280,7 +295,7 @@ def main():
 		widths = data[i,:]
 		starts= data_cum[i,:] - widths
 		
-		bar = ax.barh(range(len(yticks)), widths, left=starts, height=height, color=colors[frame_class]) # don't input label
+		bar = ax[0].barh(range(len(yticks)), widths, left=starts, height=height, color=colors[frame_class]) # don't input label
 
 	
 	#### 3_1. Evaluation Metric calc 및 barchart oob metric 삽입
@@ -301,46 +316,135 @@ def main():
 
 		Total_Evaluation_df = pd.concat([Total_Evaluation_df, Evaluation_df], ignore_index=True)
 
-		text_bar = ax.barh(i, 0, height=height) # dummy data
-		present_text(ax, text_bar, ' OOB_METRIC : {:.3f} | OVER_ESTIMATION : {:.3f} | UNDER_ESTIMATION : {:.3f} \n FN : {} | FP : {} | TN : {} | TP : {} | TOTAL : {}'.format(Evaluation_metric['OOB_metric'], Evaluation_metric['OVER_estimation'], Evaluation_metric['UNDER_estimtation'], Evaluation_metric['FN'], Evaluation_metric['FP'], Evaluation_metric['TN'], Evaluation_metric['TP'], Evaluation_metric['TOTAL']))
+		text_bar = ax[0].barh(i, 0, height=height) # dummy data
+		# present_text(ax, text_bar, ' OOB_METRIC : {:.3f} | OVER_ESTIMATION : {:.3f} | UNDER_ESTIMATION : {:.3f} \n FN : {} | FP : {} | TN : {} | TP : {} | TOTAL : {}'.format(Evaluation_metric['OOB_metric'], Evaluation_metric['OVER_estimation'], Evaluation_metric['UNDER_estimtation'], Evaluation_metric['FN'], Evaluation_metric['FP'], Evaluation_metric['TN'], Evaluation_metric['TP'], Evaluation_metric['TOTAL']))
+		present_text(ax[0], text_bar, 'OOB_METRIC : {:.3f}'.format(Evaluation_metric['OOB_metric']))
 	
 	print(Total_Evaluation_df)
 
 	# Evaluation Metric save
 	Total_Evaluation_df.to_csv(os.path.join(args.results_save_dir, '{}-{}-Evaluation.csv'.format(args.title_name, args.sub_title_name)), mode='w') # mode='w', 'a'
- 
-		
+
+	##### 3.2 section OOB Metric
+	section_oob_dict = {}
+
+	total_info_df = df(predict_data)
+	total_len = len(total_info_df)
 	
+	# init_variable
+	WINDOW_SIZE = 5000
+	INFERENCE_STEP = 5
+	slide_window_start_end_idx= [[start_idx * WINDOW_SIZE, (start_idx+1) * WINDOW_SIZE] for start_idx in range(int(math.ceil(total_len/WINDOW_SIZE)))]
+	slide_window_start_end_idx[-1][1] = total_len # last frame of last pickle
+	print(slide_window_start_end_idx)
+	
+	frame_start_idx = [start_idx * INFERENCE_STEP for start_idx, end_idx in slide_window_start_end_idx]
+	time_start_idx = [time_label[frame_label.index(idx)] for idx in frame_start_idx]
+
+	section_oob_dict = {'Frame_start_idx': frame_start_idx, 'Time_start_idx': time_start_idx}
+
+	for i, model in enumerate(yticks) :
+		print(i, model)
+		model_section_oob_metric_list = []
+
+		for start, end in slide_window_start_end_idx : # slicing
+			metric_frame_df = return_metric_frame(total_info_df.iloc[start:end, :], 'GT', model)
+
+			print(metric_frame_df)
+			
+			Evaluation_metric = calc_OOB_Evaluation_metric(len(metric_frame_df['FN_df']), len(metric_frame_df['FP_df']), len(metric_frame_df['TN_df']), len(metric_frame_df['TP_df']))
+			Evaluation_df = df(Evaluation_metric, index=[0])
+
+			model_section_oob_metric_list.append(Evaluation_metric['OOB_metric']) # section oob metric
+		
+		# oob_metric -> -1==>1변경 // TN 만 있을경우		
+		for k in range(len(model_section_oob_metric_list)) :
+			if model_section_oob_metric_list[k] == -1.0 :
+				model_section_oob_metric_list[k] = 1.0
+		
+		# model oob_metric 
+		section_oob_dict[model] = copy.deepcopy(model_section_oob_metric_list) # deep copy 
+		
+		# ranking // 가장 낮은 부분 3곳(MIN_NUM) 체크 및 표시
+		text_bar = ax[0].barh(i, 0, height=height) # dummy data
+
+		MIN_COUNT = 2
+		sort_index = np.argsort(np.array(model_section_oob_metric_list))
+
+		print(section_oob_dict[model])
+
+		for idx in sort_index[:MIN_COUNT] :
+			pos_x = frame_label.index(section_oob_dict['Frame_start_idx'][idx]) # x position
+			present_text_for_section(ax[0], text_bar, pos_x, '{:.3f}'.format(model_section_oob_metric_list[idx]))
+
+
+	# total section OOB Metric Results
+	Total_Evaluation_per_section_df = df(section_oob_dict)
+
+	print(Total_Evaluation_per_section_df)
+
+	# OOB Section Metric Save
+	Total_Evaluation_per_section_df.to_csv(os.path.join(args.results_save_dir, '{}-{}-Section_Evaluation.csv'.format(args.title_name, args.sub_title_name)), mode='w') # mode='w', 'a'
+		
+	# OOB Section Metric Plot
+	oob_section_metric_plt(Total_Evaluation_per_section_df, yticks, ax[1])
+
 	#### 4. title 설정
 	fig.suptitle(args.title_name, fontsize=18)
-	ax.set_title(args.sub_title_name)
-
-	#### 5. 
+	ax[0].set_title(args.sub_title_name)
 
 	#### 6. x축 세부설정
-	step_size = 3000 # xtick step_size
-	ax.set_xticks(range(0, len(frame_label), step_size)) # 3000
-	ax.set_xticklabels(['{}\n{}'.format(time, frame) for time, frame in zip(frame_label[::step_size], time_label[::step_size])]) # xtick change
-	ax.xaxis.set_tick_params(labelsize=7)
-	ax.set_xlabel('Frame / Time (h:m:s:fps)', fontsize=12)
+	step_size = WINDOW_SIZE # xtick step_size
+	ax[0].set_xticks(range(0, len(frame_label), step_size)) # step_size
+	ax[0].set_xticklabels(['{}\n{}'.format(time, frame) for time, frame in zip(frame_label[::step_size], time_label[::step_size])]) # xtick change
+	ax[0].xaxis.set_tick_params(labelsize=7)
+	ax[0].set_xlabel('Frame / Time (h:m:s:fps)', fontsize=12)
 	
 	#### 7. y축 세부설정
-	ax.set_yticks(range(len(yticks)))
-	ax.set_yticklabels(yticks, fontsize=10)	
-	ax.set_ylabel('Model', fontsize=12)
+	ax[0].set_yticks(range(len(yticks)))
+	ax[0].set_yticklabels(yticks, fontsize=10)	
+	ax[0].set_ylabel('Model', fontsize=12)
 	
 	#### 8. 범례 나타내기
-	box = ax.get_position() # 범례를 그래프상자 밖에 그리기위해 상자크기를 조절
-	ax.set_position([box.x0, box.y0, box.width * 0.9, box.height])
-	ax.legend(label_names, loc='center left', bbox_to_anchor=(1,0.5), shadow=True, ncol=1)
+	box = ax[0].get_position() # 범례를 그래프상자 밖에 그리기위해 상자크기를 조절
+	ax[0].set_position([box.x0, box.y0, box.width * 0.9, box.height])
+	ax[0].legend(label_names, loc='center left', bbox_to_anchor=(1,0.5), shadow=True, ncol=1)
 	
 	#### 9. 보조선(눈금선) 나타내기
-	ax.set_axisbelow(True)
-	ax.xaxis.grid(True, color='gray', linestyle='dashed', linewidth=0.5)
+	ax[0].set_axisbelow(True)
+	ax[0].xaxis.grid(True, color='gray', linestyle='dashed', linewidth=0.5)
 	
 	#### 10. 그래프 저장하고 출력하기
 	plt.savefig(os.path.join(args.results_save_dir, '{}-{}.png'.format(args.title_name, args.sub_title_name)), format='png', dpi=500)
 	plt.show()
+
+def oob_section_metric_plt(Total_Evaluation_per_section_df, model_list, ax) :
+	x_value = Total_Evaluation_per_section_df['Frame_start_idx']
+
+	for model in model_list :
+		plt.plot(x_value, Total_Evaluation_per_section_df[model], marker='o')
+
+	# sup title 설정
+	ax.set_title('OOB Metric Per Section')
+
+	# x 축 세부설정
+	ax.set_xticks(x_value)
+	ax.set_xticklabels(['{}\n{}'.format(time, frame) for time, frame in zip(x_value, Total_Evaluation_per_section_df['Time_start_idx'])]) # xtick change
+	ax.xaxis.set_tick_params(labelsize=7)
+	ax.set_xlabel('Start Frame / Time (h:m:s:fps)', fontsize=12)
+
+	# y 축 세부설정
+	ax.set_ylabel('OOB Metric', fontsize=12)
+
+	# 보조선(눈금선) 나타내기
+	ax.set_axisbelow(True)
+	ax.xaxis.grid(True, color='gray', linestyle='dashed', linewidth=0.5)
+
+
+	# 범례
+	box = ax.get_position() # 범례를 그래프상자 밖에 그리기위해 상자크기를 조절
+	ax.set_position([box.x0, box.y0, box.width * 0.9, box.height])
+	ax.legend(model_list, loc='center left', bbox_to_anchor=(1,0.5), shadow=True, ncol=1)
 
 
 if __name__=='__main__':
