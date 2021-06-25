b9f4f525dbca6608e8c47f54c9791bd257357754
Hyeongyuc96Hutom <hyeongyuc96@hutom.co.kr>
2021-06-25 10:06:33 +0900

    add support model and sequence for gradcam

--- visual_gradcam.sh
+++ visual_gradcam.sh
@@ -11,6 +11,10 @@ parser.add_argument('--inference_img_dir', type=str, help='root dir for inferenc
 parser.add_argument('--save_dir', type=str, help='gradcam results save path')
 END
 
+
+: << "END"
+# old version
+
 model_name="wide_resnet50_2"
 
 python visual_gradcam.py \
@@ -19,5 +23,51 @@ python visual_gradcam.py \
 --model_name ${model_name} \
 --inference_img_dir "./results-robot_oob-wide_resnet50_2-fold_2-last/R006/R006_ch1_video_01/fp_frame" \
 --save_dir "./gradcam_results"
+END
 
 
+model_name="efficientnet_b3"
+title_name="${model_name}_FP"
+model_path="./logs/ROBOT/OOB/robot-oob-0610-fold_1/ckpoint_robot-oob-0610-fold_1-model=efficientnet_b3-batch=32-lr=0.001-fold=1-ratio=3-epoch=24-last.ckpt" \
+video_dir="/data/ROBOT/Video"
+consensus_results_path="./results/results-robot-oob-0610-efficientnet_b3-fold_1-last/R_22/Inference-ROBOT-R_22.csv"
+save_dir="./new_gradcam_results/${title_name}"
+
+python visual_gradcam.py \
+--title_name ${title_name} \
+--model_name ${model_name} \
+--model_path ${model_path} \
+--video_dir ${video_dir} \
+--consensus_results_path ${consensus_results_path} \
+--save_dir ${save_dir}
+
+
+model_name="efficientnet_b3"
+title_name="${model_name}_FP"
+model_path="./logs/ROBOT/OOB/robot-oob-0610-fold_3/ckpoint_robot-oob-0610-fold_3-model=efficientnet_b3-batch=32-lr=0.001-fold=3-ratio=3-epoch=24-last.ckpt" \
+video_dir="/data/ROBOT/Video"
+consensus_results_path="./results/results-robot-oob-0610-efficientnet_b3-fold_3-last/R_7/Inference-ROBOT-R_7.csv"
+save_dir="./new_gradcam_results/${title_name}"
+
+python visual_gradcam.py \
+--title_name ${title_name} \
+--model_name ${model_name} \
+--model_path ${model_path} \
+--video_dir ${video_dir} \
+--consensus_results_path ${consensus_results_path} \
+--save_dir ${save_dir}
+
+model_name="efficientnet_b3"
+title_name="${model_name}_FP"
+model_path="./logs/ROBOT/OOB/robot-oob-0610-fold_3/ckpoint_robot-oob-0610-fold_3-model=efficientnet_b3-batch=32-lr=0.001-fold=3-ratio=3-epoch=24-last.ckpt" \
+video_dir="/data/ROBOT/Video"
+consensus_results_path="./results/results-robot-oob-0610-efficientnet_b3-fold_3-last/R_19/Inference-ROBOT-R_19.csv"
+save_dir="./new_gradcam_results/${title_name}"
+
+python visual_gradcam.py \
+--title_name ${title_name} \
+--model_name ${model_name} \
+--model_path ${model_path} \
+--video_dir ${video_dir} \
+--consensus_results_path ${consensus_results_path} \
+--save_dir ${save_dir}
