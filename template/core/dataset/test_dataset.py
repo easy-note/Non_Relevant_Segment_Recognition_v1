bea81def99799734f5f4b12da6c64f947f081671
hyeongyuc <hyeongyuc96@hutom.co.kr>
2021-09-23 19:33:54 +0900

    add new test_dataset for DB

---
+++ template/core/dataset/test_dataset.py
@@ -0,0 +1,63 @@
+import os
+import glob
+
+import torch
+from torch.utils.data import Dataset, DataLoader
+from torchvision import transforms
+
+from PIL import Image
+from natsort import natsorted
+
+from core.config.data_info import data_transforms
+
+class DBDataset(Dataset): 
+
+    def __init__(self, DB_path): 
+        self.img_list = glob.glob(os.path.join(DB_path, '*.jpg')) # ALL img into DB path
+        self.img_list = natsorted(self.img_list) # sorting
+
+        self.aug = data_transforms['test']
+    
+    def __len__(self):
+        return len(self.img_list)
+
+    def __getitem__(self, index):
+        img_path = self.img_list[index]
+
+        img = Image.open(img_path)
+        img = self.aug(img)
+
+        # parsing DB img idx
+        video_name, idx = os.path.splitext(os.path.basename(img_path))[0].split('-') # ~/01_G_01_R_999_ch1_1-0000000001.jpg
+        
+
+        return {'img': img,
+                'DB_idx': idx,
+                'img_path': img_path}
+
+
+# IT'S NOT UNIFORM SAMPLER
+# test => [0,5,10,15...] step size
+# gradcam => [0, 9, 11, 13 ...]
+# https://stackoverflow.com/questions/66065272/customizing-the-batch-with-specific-elements
+# Dataloader(DB_Dataset, batch_sampler=[[0,1,2], [3,4,5], [6,7], [8,9]])
+class IDXSampler():
+    def __init__(self, idx_list, batch_size):
+        self.idx_list = idx_list
+        self.batch_size = batch_size
+    
+    def __iter__(self):
+        batches = []
+
+        start_pos = 0
+        end_pos = len(self.idx_list)
+
+        for _ in range(start_pos, end_pos + self.batch_size, self.batch_size):
+            batch_idx = self.idx_list[start_pos : start_pos + self.batch_size]
+
+            if batch_idx != []:
+                batches.append(batch_idx)
+            
+            start_pos = start_pos + self.batch_size
+        
+        return iter(batches)
