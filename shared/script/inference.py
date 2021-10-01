8be2591b0733fe0f980a10a6f48ca4578244975b
Hyeongyuc96Hutom <hyeongyuc96@hutom.co.kr>
2021-10-01 18:49:00 +0900

    write inference script as same as vihub pro flow

---
+++ shared/script/inference.py
@@ -0,0 +1,145 @@
+### only using shared inference module as same as VIHUB PRO
+if __package__ is None:
+    import sys
+    import os
+
+    root_path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    sys.path.append(root_path)
+    sys.path.append(os.path.join(root_path, 'core'))
+    sys.path.append(os.path.join(root_path, 'core', 'api'))
+    sys.path.append(os.path.join(root_path, 'core', 'api', 'Evaluation_vihub'))
+    sys.path.append(os.path.join(root_path, 'core', 'api', 'Inference_vihub'))
+    sys.path.append(os.path.join(root_path, 'core', 'api', 'Inference_vihub', 'VIHUB_pro_QA_v2'))
+
+    sys.path.append(os.path.join(root_path, 'core', 'utils'))
+
+    print(sys.path)
+
+
+import os
+import sys
+import argparse
+import pandas as pd
+
+from core.api.Inference_vihub.VIHUB_pro_QA_v2.test import inference_by_frame_list # inference module
+from core.api.Evaluation_vihub.OOB_inference_module import Inference_eval # evaluation module
+from core.api.Evaluation_vihub.OOB_Post_Procssing_module import FilterBank # PP module
+from core.utils.ffmpegHelper import ffmpegHelper # ffmpeg module
+from core.utils.evalHelper import evalHelper # eval Helper
+from core.utils.visualHelper import visualHelper # visual Helper
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument('--model_path', type=str, help='model_root_path')
+parser.add_argument('--video_path', type=str, help='input video path')
+parser.add_argument('--results_path', type=str, help='saving results path for predict vector')
+parser.add_argument('--gt_path', type=str, help='annotation json path')
+parser.add_argument('--pp', action='store_true', help='If True, apply pp')
+
+args, _ = parser.parse_known_args()
+
+
+'''
+# INFERENCE RESULT FOLDER HIREACHY
+    results/
+        frames/
+            frame00
+            frame01 ...
+            ...
+        mobilenet/
+            mobilenet.csv -> p1 path
+            mobilenet.json # <- gt, predict length diff -> ???? # input predict list, gt list 
+        efficientnet/
+            efficientnet.csv -> p2 path
+            efficientnet.json # <- gt, predict length diff -> ???? # input predict list, gt list
+        visual/
+            visual.png
+            mobilenet/
+                mobilenet_FP.gif
+                mobilenet_FN.gif
+            efficientnet/
+                efficientnet_FP.gif
+                efficientnet_FN.gif
+'''
+def inference_using_module(model_path, video_path, results_path, gt_path, pp):
+
+    # 1.set path
+    ## set model path
+    mobilenet_model_path = os.path.join(model_path, 'mobilenet_v3_large.ckpt')
+    efficientnet_model_path = os.path.join(model_path, 'efficientnet_b3.ckpt')
+
+    ## set cutted frame path
+    cutted_frame_path = os.path.join(results_path, 'frame')
+    os.makedirs(cutted_frame_path, exist_ok=True)
+
+    ## set results dir
+    mobilenet_results_dir = os.path.join(results_path, 'mobilenet')
+    efficientnet_results_dir = os.path.join(results_path, 'efficient')
+    os.makedirs(mobilenet_results_dir, exist_ok=True)
+    os.makedirs(efficientnet_results_dir, exist_ok=True)
+
+    visual_dir = os.path.join(results_path, 'visual')
+    os.makedirs(visual_dir, exist_ok=True)
+
+    ## set results file path
+    mobilenet_predict_path = os.path.join(mobilenet_results_dir, 'mobilenet.csv')
+    efficientnet_predict_path = os.path.join(efficientnet_results_dir, 'efficientnet.csv')
+
+    mobilenet_eval_path = os.path.join(mobilenet_results_dir, 'mobilenet.json')
+    efficientnet_eval_path = os.path.join(efficientnet_results_dir, 'efficientnet.json')
+
+    # 2. frame cutting module
+    '''
+    ffmpeg = ffmpegHelper(video_path, cutted_frame_path)
+    ffmpeg.cut_frame_1fps()
+    '''
+
+    # 3.module test
+    ## [1] inference module
+    '''
+    mobilenet_predict_list = inference_by_frame_list(cutted_frame_path, mobilenet_model_path) # mobilenet
+    efficient_predict_list = inference_by_frame_list(cutted_frame_path, efficientnet_model_path) # efficient
+
+    ## (save predict ector to csv)
+    pd.DataFrame({'predict': mobilenet_predict_list}).to_csv(mobilenet_predict_path)
+    pd.DataFrame({'predict': efficient_predict_list}).to_csv(efficientnet_predict_path)
+    '''
+
+    print('----')
+    mobilenet_predict_list = list(pd.read_csv(mobilenet_predict_path)['predict'])
+    efficient_predict_list = list(pd.read_csv(efficientnet_predict_path)['predict'])
+
+    ##### evaluation Helper [strat] #####
+    mobile_evalHelper = evalHelper(mobilenet_predict_path, gt_path, inference_step=30) # mobilenet
+    efficient_evalHelper = evalHelper(efficientnet_predict_path, gt_path, inference_step=30) # efficient
+
+
+    gt_list, mobilenet_predict_list= mobile_evalHelper.load_gt_list_predict_list()
+    gt_list, efficient_predict_list= efficient_evalHelper.load_gt_list_predict_list()
+    ##### evaluation Helper [end] #####
+
+    print(mobilenet_predict_list)
+
+    ## [1.5] pp module
+    if pp == True :
+        print('== APPLY PP ==')
+        mobilenet_predict_list = FilterBank(mobilenet_predict_list, seq_fps=1).apply_best_filter()
+        efficient_predict_list = FilterBank(efficient_predict_list, seq_fps=1).apply_best_filter()
+        print('== PP DONE ==')
+
+    ## [2] evaluation module
+    print('mobile : ', Inference_eval(mobilenet_predict_path, gt_path, mobilenet_eval_path, inference_step=30).calc_OR_CR()) # mobilenet
+    print('efficient : ', Inference_eval(efficientnet_predict_path, gt_path, efficientnet_eval_path, inference_step=30).calc_OR_CR()) # efficient
+
+    ## [3] visual module
+    visual_helper = visualHelper(gt_list, mobilenet_predict_list, efficient_predict_list, visual_dir, frame_path=cutted_frame_path) # visual (mobile, efficient)
+    visual_helper.inference_visual()
+    visual_helper.generate_gif() # FP FN gif (mobile, efficient)
+
+if __name__ == '__main__':
+
+    os.environ['CUDA_VISIBLE_DEVICES'] = '0,1'
+
+    inference_using_module(args.model_path, args.video_path, args.results_path, args.gt_path, args.pp)
+
+
---
+++ shared/script/inference.sh
@@ -0,0 +1,111 @@
+### apply pp
+python inference.py \
+    --model_path "/OOB_RECOG/shared/model" \
+    --video_path "/data2/oob_module_test_results/2021-09-24/49682073-40d4-46ac-9459-d906a5983d1d/01_G_01_R_100_ch1_01.mp4" \
+    --results_path "/data2/oob_module_test_temp/R_100/01_G_01_R_100_ch1_01" \
+    --gt_path "/data2/oob_module_test_results/2021-09-24/49682073-40d4-46ac-9459-d906a5983d1d/01_G_01_R_100_ch1_01_OOB_27.json" \
+    --pp
+
+: <<'END'
+### non pp
+python inference.py \
+    --model_path "/OOB_RECOG/shared/model" \
+    --video_path "/data2/oob_module_test_results/2021-09-24/49682073-40d4-46ac-9459-d906a5983d1d/01_G_01_R_100_ch1_01.mp4" \
+    --results_path "/data2/oob_module_test_linux/R_100/01_G_01_R_100_ch1_01" \
+    --gt_path "/data2/oob_module_test_results/2021-09-24/49682073-40d4-46ac-9459-d906a5983d1d/01_G_01_R_100_ch1_01_OOB_27.json"
+
+
+python inference.py \
+    --model_path "/OOB_RECOG/shared/model" \
+    --video_path "/data2/oob_module_test_results/2021-09-24/49682073-40d4-46ac-9459-d906a5983d1d/01_G_01_R_100_ch1_01.mp4" \
+    --results_path "/data2/oob_module_test_linux/R_100/01_G_01_R_100_ch1_01" \
+    --gt_path "/data2/oob_module_test_results/2021-09-24/49682073-40d4-46ac-9459-d906a5983d1d/01_G_01_R_100_ch1_01_OOB_27.json"
+
+python inference.py \
+    --model_path "/OOB_RECOG/shared/model" \
+    --video_path "/data2/oob_module_test_results/2021-09-24/b96a9f4a-555e-44bc-b2dc-645eac1003e3/01_G_01_R_100_ch1_03.mp4" \
+    --results_path "/data2/oob_module_test_linux/R_100/01_G_01_R_100_ch1_03" \
+    --gt_path "/data2/oob_module_test_results/2021-09-24/b96a9f4a-555e-44bc-b2dc-645eac1003e3/01_G_01_R_100_ch1_03_OOB_27.json"
+
+python inference.py \
+    --model_path "/OOB_RECOG/shared/model" \
+    --video_path "/data2/oob_module_test_results/2021-09-24/b1820411-6299-47e4-8ac5-03ecf4e40561/01_G_01_R_100_ch1_05.mp4" \
+    --results_path "/data2/oob_module_test_linux/R_100/01_G_01_R_100_ch1_05" \
+    --gt_path "/data2/oob_module_test_results/2021-09-24/b1820411-6299-47e4-8ac5-03ecf4e40561/01_G_01_R_100_ch1_05_OOB_27.json"
+
+python inference.py \
+    --model_path "/OOB_RECOG/shared/model" \
+    --video_path "/data2/oob_module_test_results/2021-09-28/8628f15b-52ac-4a11-9880-7f2ae6083049/01_G_01_R_301_ch1_01.mp4" \
+    --results_path "/data2/oob_module_test_linux/R_301/01_G_01_R_301_ch1_01" \
+    --gt_path "/data2/oob_module_test_results/2021-09-28/8628f15b-52ac-4a11-9880-7f2ae6083049/01_G_01_R_301_ch1_01_OOB_28.json"
+
+
+python inference.py \
+    --model_path "/OOB_RECOG/shared/model" \
+    --video_path "/data2/oob_module_test_results/2021-09-28/53f6c884-9762-44dd-8ce7-967e47b7aa40/01_G_01_R_301_ch1_04.mp4" \
+    --results_path "/data2/oob_module_test_linux/R_301/01_G_01_R_301_ch1_04" \
+    --gt_path "/data2/oob_module_test_results/2021-09-28/53f6c884-9762-44dd-8ce7-967e47b7aa40/01_G_01_R_301_ch1_04_OOB_28.json"
+
+
+python inference.py \
+    --model_path "/OOB_RECOG/shared/model" \
+    --video_path "/data2/oob_module_test_results/2021-09-28/e87840f8-eecb-48ff-8b01-bad0a3b9fd44/01_G_01_R_312_ch1_02.mp4" \
+    --results_path "/data2/oob_module_test_linux/R_312/01_G_01_R_312_ch1_02" \
+    --gt_path "/data2/oob_module_test_results/2021-09-28/e87840f8-eecb-48ff-8b01-bad0a3b9fd44/01_G_01_R_312_ch1_02_OOB_27.json"
+
+
+python inference.py \
+    --model_path "/OOB_RECOG/shared/model" \
+    --video_path "/data2/oob_module_test_results/2021-09-28/40c1ce25-9389-4f54-a75f-1bf79147cc78/01_G_01_R_312_ch1_03.mp4" \
+    --results_path "/data2/oob_module_test_linux/R_312/01_G_01_R_312_ch1_03" \
+    --gt_path "/data2/oob_module_test_results/2021-09-28/40c1ce25-9389-4f54-a75f-1bf79147cc78/01_G_01_R_312_ch1_03_OOB_27.json"
+
+
+python inference.py \
+    --model_path "/OOB_RECOG/shared/model" \
+    --video_path "/data2/oob_module_test_results/2021-09-28/4525ee57-e9b3-4621-9de3-e8d20aeba766/01_G_01_R_405_ch1_01.mp4" \
+    --results_path "/data2/oob_module_test_linux/R_405/01_G_01_R_405_ch1_01" \
+    --gt_path "/data2/oob_module_test_results/2021-09-28/4525ee57-e9b3-4621-9de3-e8d20aeba766/01_G_01_R_405_ch1_01_OOB_29.json"
+
+
+python inference.py \
+    --model_path "/OOB_RECOG/shared/model" \
+    --video_path "/data2/oob_module_test_results/2021-09-28/6c6c0490-3019-4306-a722-5a98bafbe0ec/01_G_01_R_405_ch1_03.mp4" \
+    --results_path "/data2/oob_module_test_linux/R_405/01_G_01_R_405_ch1_03" \
+    --gt_path "/data2/oob_module_test_results/2021-09-28/6c6c0490-3019-4306-a722-5a98bafbe0ec/01_G_01_R_405_ch1_03_OOB_29.json"
+
+python inference.py \
+    --model_path "/OOB_RECOG/shared/model" \
+    --video_path "/data2/oob_module_test_results/2021-09-28/fe085771-386d-4d0e-a4d5-d4c12854d985/01_G_01_R_405_ch1_05.mp4" \
+    --results_path "/data2/oob_module_test_linux/R_405/01_G_01_R_405_ch1_05" \
+    --gt_path "/data2/oob_module_test_results/2021-09-28/fe085771-386d-4d0e-a4d5-d4c12854d985/01_G_01_R_405_ch1_05_OOB_29.json"
+
+
+python inference.py \
+    --model_path "/OOB_RECOG/shared/model" \
+    --video_path "/data2/oob_module_test_results/2021-09-28/3c2704b7-903a-42a1-a20a-9a2baf2dc446//01_G_01_R_418_ch1_02.mp4" \
+    --results_path "/data2/oob_module_test_linux/R_418/01_G_01_R_418_ch1_02" \
+    --gt_path "/data2/oob_module_test_results/2021-09-28/3c2704b7-903a-42a1-a20a-9a2baf2dc446/01_G_01_R_418_ch1_02_OOB_28.json"
+
+
+python inference.py \
+    --model_path "/OOB_RECOG/shared/model" \
+    --video_path "/data2/oob_module_test_results/2021-09-28/8d880f7e-88e8-4dfa-9089-90f2996686e5/01_G_01_R_418_ch1_04.mp4" \
+    --results_path "/data2/oob_module_test_linux/R_418/01_G_01_R_418_ch1_04" \
+    --gt_path "/data2/oob_module_test_results/2021-09-28/8d880f7e-88e8-4dfa-9089-90f2996686e5/01_G_01_R_418_ch1_04_OOB_28.json"
+
+
+python inference.py \
+    --model_path "/OOB_RECOG/shared/model" \
+    --video_path "/data2/oob_module_test_results/2021-09-28/723c5a8c-e86e-4c6f-a72c-e99633288870/01_G_01_R_418_ch1_06.mp4" \
+    --results_path "/data2/oob_module_test_linux/R_418/01_G_01_R_418_ch1_06" \
+    --gt_path "/data2/oob_module_test_results/2021-09-28/723c5a8c-e86e-4c6f-a72c-e99633288870/01_G_01_R_418_ch1_06_OOB_28.json"
+
+
+python inference.py \
+    --model_path "/OOB_RECOG/shared/model" \
+    --video_path "/data2/oob_module_test_results/2021-09-28/3b079065-48b3-443b-8156-b391ff0659c4/01_G_01_R_526_ch1_01.mp4" \
+    --results_path "/data2/oob_module_test_linux/R_526/01_G_01_R_526_ch1_01" \
+    --gt_path "/data2/oob_module_test_results/2021-09-28/3b079065-48b3-443b-8156-b391ff0659c4/01_G_01_R_526_ch1_01_OOB_28.json"
+END
+
