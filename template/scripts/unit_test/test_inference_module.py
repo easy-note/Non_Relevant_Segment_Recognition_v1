bd0df1a0837e00e7a240e3341bd5a8a073fede06
hyeongyuc <hyeongyuc96@hutom.co.kr>
2021-10-06 15:13:59 +0900

    add use case for inference module test

--- template/scripts/unit_test/test_inference_module.py
+++ template/scripts/unit_test/test_inference_module.py
@@ -15,7 +15,7 @@ def main():
         
 
     parser.add_argument('--inference_interval', type=int, 
-                        default=30,
+                        default=1,
                         help='Inference Interval of frame')
 
     parser.add_argument('--inference_fold',
@@ -35,20 +35,36 @@ def main():
     model_path = '/OOB_RECOG/model_ckpt/ckpoint_0816-test-mobilenet_v3_large-model=mobilenet_v3_large-batch=32-lr=0.001-fold=1-ratio=3-epoch=24-last.ckpt'
     model = CAMIO.load_from_checkpoint(model_path, args=args)
     '''
-
+    
+    # use case 1 - init Inference
     db_path = '/data3/OOB_Recog/img_db/ROBOT/R_100/01_G_01_R_100_ch1_01' # R_100_ch1_01
     
-
     Inference = InferenceDB(model, db_path, args.inference_interval) # Inference object
-    # Inference.set_inference_interval(args.inference_interval) # you can also set inference interval
     predict_list, target_img_list, target_frame_idx_list = Inference.start() # call start
 
     # Inference module results
-    print(len(predict_list))
     print(predict_list)
-
     print(target_img_list)
     print(target_frame_idx_list)
+
+    # use case 2 - set db path
+    Inference.set_db_path('/data3/OOB_Recog/img_db/ROBOT/R_100/01_G_01_R_100_ch1_03') # you can also change db_path
+    predict_list, target_img_list, target_frame_idx_list = Inference.start() # call start
+
+    # Inference module results
+    print(predict_list)
+    print(target_img_list)
+    print(target_frame_idx_list)
+
+    # use case 3 - set inference_interval
+    Inference.set_inference_interval(30) # you can also set inference interval
+    predict_list, target_img_list, target_frame_idx_list = Inference.start() # call start
+
+    # Inference module results
+    print(predict_list)
+    print(target_img_list)
+    print(target_frame_idx_list)
+    
 
 if __name__ == '__main__':
     
