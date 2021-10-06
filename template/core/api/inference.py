31e0586240c76a403d7972d4e7c6462ee21b4072
hyeongyuc <hyeongyuc96@hutom.co.kr>
2021-10-06 15:12:55 +0900

    modify inference module for re-PR [remove batchsampler, seperate gen set method]

--- template/core/api/inference.py
+++ template/core/api/inference.py
@@ -9,39 +9,43 @@ class InferenceDB(): # InferenceDB
         Inference per DB from model
     """ 
 
-    def __init__(self, model, db_path, inference_interval=30):
+    def __init__(self, model, db_path, inference_interval=1):
         self.model = model
         self.model = self.model.eval()
 
         self.db_path = db_path
 
-        self.test_dataset = DBDataset(db_path)
+        self.inference_interval = inference_interval
 
-        self.batch_sampler = self.get_batch_sampler(inference_interval) # default interval = 30
+    def set_db_path(self, db_path):
+        self.db_path = db_path
 
     def set_inference_interval(self, inference_interval):
-        self.batch_sampler = self.get_batch_sampler(inference_interval)
+        self.inference_interval = inference_interval
 
-    def get_batch_sampler(self, inference_interval):
-        return BatchSampler(sampler=IntervalSampler(self.test_dataset, interval=inference_interval),
-                            batch_size=128, drop_last=False) # default batch_size=128
+    def _gen_dataset(self):
+        return DBDataset(self.db_path)
+
+    def _gen_sampler(self):
+        return IntervalSampler(self._gen_dataset(), interval=self.inference_interval)
 
     def start(self): 
         print('\n\t########## INFERENCEING (DB) #########\n')
 
         # DB dataset
-        print('DB PATH : ', self.db_path)
+        print('DB PATH : \t\t\t{}'.format(self.db_path))
+        print('INFERENCE_INTERVAL : \t\t{}'.format(self.inference_interval))
 
         # data loder
-        dl = DataLoader(dataset=self.test_dataset,
-                        batch_sampler=self.batch_sampler)
+        dl = DataLoader(dataset=self._gen_dataset(),
+                        sampler=self._gen_sampler(),
+                        batch_size=128)
         
         # inference log
         predict_list = []
         target_img_list = []
         target_frame_idx_list = []
 
-        cnt = 0
         # inferencing model
         with torch.no_grad() :
             for sample in tqdm(dl, desc='Inferencing... \t ==> {}'.format(self.db_path)) :
@@ -55,7 +59,7 @@ class InferenceDB(): # InferenceDB
                 # save results
                 predict_list += list(batch_predict)
                 target_img_list += sample['img_path'] # target img path
-                target_frame_idx_list += sample['db_idx'] # target DB index
+                target_frame_idx_list += sample['db_idx'] # target DB
 
         target_frame_idx_list = list(map(int, target_frame_idx_list)) # '0000000001' -> 1
 
