d8761459dea75b372742686f741c69b577bd4ec9
jihyun98hutom <jihyun.lee@hutom.io>
2021-10-13 14:27:27 +0900

    Implement generate_oob_assets

---
+++ template/core/config/assets_info.py
@@ -0,0 +1,26 @@
+
+"""
+    annotation, img_db path
+    oob_assets save path
+"""
+
+annotation_path = {
+    'annotation_v1_base_path': '/data2/Public/IDC_21.06.25/ANNOTATION/Gastrectomy/Event/OOB/V1',
+    'annotation_v2_base_path': '/data2/Public/IDC_21.06.25/ANNOTATION/Gastrectomy/Event/OOB/V2',
+    'annotation_v3_base_path': '/nas2/Public/IDC_21.06.25/ANNOTATION/Gastrectomy/Event/OOB/V3/TBE'
+}
+
+img_db_path = {
+    '12': '/raid/img_db'
+}
+
+oob_assets_save_path = {
+    'oob_assets_v1_robot_save_path':  '/data2/Public/OOB_Recog/oob_assets/V1/ROBOT',
+    'oob_assets_v1_lapa_save_path':'/data2/Public/OOB_Recog/oob_assets/V1/LAPA',
+
+    'oob_assets_v2_robot_save_path':'/raid/img_db/oob_assets/V2/ROBOT',
+    'oob_assets_v2_lapa_save_path': '/raid/img_db/oob_assets/V2/LAPA',
+
+    'oob_assets_v3_robot_save_path': '/raid/img_db/oob_assets/V3/ROBOT'
+}
+
