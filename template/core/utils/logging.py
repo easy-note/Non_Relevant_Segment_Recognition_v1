45590ff0b23442129859f3c71f4c830f25cb2a93
hyeongyuc <hyeongyuc96@hutom.co.kr>
2021-10-26 18:45:12 +0900

    add new utils for save experiments results

--- template/core/utils/logging.py
+++ template/core/utils/logging.py
@@ -1,5 +1,8 @@
 import time
 import os
+import pandas as pd
+
+from core.utils.parser import FileLoader # file load helper
 
 class LogHelper():
     """
@@ -36,3 +39,75 @@ class LogHelper():
     def save(self, logging):
         with open(self.save_path, 'a') as f :
             f.write(logging)
+
+
+class ReportHelper():
+    # inference_main return으로 experiments 결과 저장, 저장하기 위해 experiemnts results sheet(csv) path가 args에 포함되어 있어야 하지 않을까?
+    def __init__(self, report_save_path, report_type):
+        self.report_save_path = report_save_path 
+        self.report_type = report_type # .csv
+
+        support_report_type = ['experiments', 'patients', 'videos']
+        assert report_type in support_report_type, 'NOT SUPPORT REPORT TYPE'
+
+    def _report_form_sanity_check(self, report_form):
+        assert set(self.get_report_form().keys()) == set(report_form.keys()), 'report form is not sanity'
+
+    def get_report_form(self):
+        init_report_form = { # one-columne of experiments report
+            'experiments': {
+                'model':'',
+                'method':'',
+                'inference_fold':'',
+                'mCR':0,
+                'mOR':0,
+                'CR':0,
+                'OR':0,
+            },
+
+            'patients': { # one-columne of inference report (each patients)
+                'Patient' : '',
+                'FP' : 0,
+                'TP' : 0,
+                'FN' : 0,
+                'TN' : 0,
+                'TOTAL' : 0,
+                'CR' : 0,
+                'OR' : 0,
+            },
+
+            'videos': { # one-columne of inference report (each videos)
+                'Patient' : '',
+                'Video' : '',
+                'FP' : 0,
+                'TP' : 0,
+                'FN' : 0,
+                'TN' : 0,
+                'TOTAL' : 0,
+                'CR' : 0,
+                'OR' : 0,
+            }
+        }
+
+        return init_report_form[self.report_type]
+
+    def save_report(self, report_form):
+        self._report_form_sanity_check(report_form) # sanity check : input(report_form)'s keys should be same as init_report_forms's keys
+        report_df = pd.DataFrame.from_dict([report_form]) # dict to df
+        report_df = report_df.reset_index(drop=True)
+
+        merged_df = report_df
+        if os.path.isfile(self.report_save_path): # append
+            f_loader = FileLoader()
+            f_loader.set_file_path(self.report_save_path)
+            saved_report_df = f_loader.load()
+
+            saved_report_df.drop(['Unnamed: 0'], axis = 1, inplace = True) # to remove Unmaned : 0 colume
+
+            merged_df = pd.concat([saved_report_df, report_df], ignore_index=True, sort=False)
+            
+        merged_df.to_csv(self.report_save_path, mode='w')
+
+        print(merged_df)
+
+        return report_df
