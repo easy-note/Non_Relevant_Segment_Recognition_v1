0e8b100bc723ab9766c4ca890dd977f061718926
bgpark1285 <bgpark@hutom.io>
2021-11-19 09:23:17 +0900

    visual tools merged

--- template/scripts/test.sh
+++ template/scripts/test.sh
@@ -82,10 +82,25 @@
 
 
 nohup python visual_flow.py --fold '1' --trial 1 --model "mobilenetv3_large_100" --max_epoch 100 \
---batch_size 256 --lr_scheduler "step_lr" --lr_scheduler_step 5 --lr_scheduler_factor 0.9 --cuda_list '4' \
+--batch_size 256 --lr_scheduler "step_lr" --lr_scheduler_step 5 --lr_scheduler_factor 0.9 --cuda_list '1' \
+--IB_ratio 3 --random_seed 3829 --stage 'hem_train' --hem_extract_mode 'hem-emb-online' --inference_fold '1' \
+--emb_type 2 --save_path '/code/OOB_Recog/logs/hem-online-3-1-step-emb-proxy-all' > /dev/null &
+
+
+nohup python visual_flow.py --fold '1' --trial 1 --model "mobilenetv3_large_100" --max_epoch 100 \
+--batch_size 256 --lr_scheduler "step_lr" --lr_scheduler_step 5 --lr_scheduler_factor 0.9 --cuda_list '1' \
 --IB_ratio 3 --random_seed 3829 --stage 'hem_train' --hem_extract_mode 'hem-emb-online' --inference_fold '1' \
 --use_wise_sample \
---sampling_type 1 --emb_type 2 --save_path '/code/OOB_Recog/logs/hem-online-3-1-step-ws-emb' > /dev/null &
+--emb_type 2 --save_path '/code/OOB_Recog/logs/hem-online-3-1-step-ws-emb-proxy-all' > /dev/null &
+
+
+nohup python visual_flow.py --fold '1' --trial 1 --model "mobilenetv3_large_100" --max_epoch 100 \
+--batch_size 256 --lr_scheduler "step_lr" --lr_scheduler_step 5 --lr_scheduler_factor 0.9 --cuda_list '1' \
+--IB_ratio 3 --random_seed 3829 --stage 'hem_train' --hem_extract_mode 'hem-emb-online' --inference_fold '1' \
+--use_wise_sample \
+--sampling_type 1 --emb_type 2 --save_path '/code/OOB_Recog/logs/hem-online-3-1-step-ws-emb-all-loss' > /dev/null &
+
+
 
 
 nohup python visual_flow.py --fold '1' --trial 1 --model "mobilenetv3_large_100" --max_epoch 100 \
@@ -93,3 +108,75 @@ nohup python visual_flow.py --fold '1' --trial 1 --model "mobilenetv3_large_100"
 --IB_ratio 3 --random_seed 3829 --stage 'hem_train' --hem_extract_mode 'hem-focus-online' --inference_fold '1' \
 --use_wise_sample \
 --sampling_type 1 --emb_type 2 --save_path '/code/OOB_Recog/logs/hem-online-3-1-step-ws-focus' > /dev/null &
+
+
+
+nohup python visual_flow.py --fold '1' --trial 1 --model "mobilenetv3_large_100" --max_epoch 100 \
+--batch_size 256 --lr_scheduler "step_lr" --lr_scheduler_step 5 --lr_scheduler_factor 0.9 --cuda_list '4' \
+--IB_ratio 4 --random_seed 3829 --stage 'hem_train' --hem_extract_mode 'hem-emb-online' --inference_fold '1' \
+--use_wise_sample \
+--sampling_type 1 --emb_type 2 --save_path '/code/OOB_Recog/logs/hem-online-4-1-step-ws-emb' > /dev/null &
+
+
+nohup python visual_flow.py --fold '1' --trial 1 --model "mobilenetv3_large_100" --max_epoch 100 \
+--batch_size 256 --lr_scheduler "step_lr" --lr_scheduler_step 5 --lr_scheduler_factor 0.9 --cuda_list '3' \
+--IB_ratio 5 --random_seed 3829 --stage 'hem_train' --hem_extract_mode 'hem-emb-online' --inference_fold '1' \
+--use_wise_sample \
+--sampling_type 1 --emb_type 2 --save_path '/code/OOB_Recog/logs/hem-online-5-1-step-ws-emb' > /dev/null &
+
+
+
+
+nohup python inference_only.py --fold '1' --trial 1 --model "mobilenetv3_large_100" --max_epoch 100 \
+--batch_size 256 --lr_scheduler "step_lr" --lr_scheduler_step 5 --lr_scheduler_factor 0.9 --cuda_list '0' \
+--IB_ratio 3 --random_seed 3829 --stage 'hem_train' --hem_extract_mode 'hem-emb-online' --inference_fold '1' \
+--use_online_mcd --emb_type 2 --save_path '/code/OOB_Recog/logs/hem-online-3-1-step-emb-mcd' \
+--restore_path '/code/OOB_Recog/logs/hem-online-3-1-step-emb-mcd-trial:1-fold:1/TB_log/version_0' > /dev/null &
+
+
+
+nohup python inference_only.py --fold '1' --trial 1 --model "mobilenetv3_large_100" --max_epoch 100 \
+--batch_size 256 --lr_scheduler "step_lr" --lr_scheduler_step 5 --lr_scheduler_factor 0.9 --cuda_list '0' \
+--IB_ratio 3 --random_seed 3829 --stage 'hem_train' --hem_extract_mode 'hem-emb-online' --inference_fold '1' \
+--emb_type 2 --save_path '/code/OOB_Recog/logs/hem-online-3-1-step-emb-proxy-all' \
+--restore_path '/code/OOB_Recog/logs/hem-online-3-1-step-emb-proxy-all-trial:1-fold:1/TB_log/version_0' > /dev/null &
+
+
+
+
+
+
+nohup python visual_flow.py --fold '1' --trial 1 --model "mobilenetv3_large_100" --max_epoch 100 \
+--batch_size 256 --lr_scheduler "step_lr" --lr_scheduler_step 5 --lr_scheduler_factor 0.9 --cuda_list '0' \
+--IB_ratio 3 --random_seed 3829 --stage 'hem_train' --hem_extract_mode 'hem-emb-online' --inference_fold '1' \
+--use_wise_sample \
+--emb_type 2 --save_path '/code/OOB_Recog/logs/hem-online-3-1-step-ws-emb-proxy-all' > /dev/null &
+
+
+nohup python visual_flow.py --fold '1' --trial 1 --model "mobilenetv3_large_100" --max_epoch 100 \
+--batch_size 256 --lr_scheduler "step_lr" --lr_scheduler_step 5 --lr_scheduler_factor 0.9 --cuda_list '1' \
+--IB_ratio 3 --random_seed 3829 --stage 'hem_train' --hem_extract_mode 'hem-emb-online' --inference_fold '1' \
+--emb_type 2 --save_path '/code/OOB_Recog/logs/hem-online-3-1-step-emb-half' > /dev/null &
+
+
+nohup python visual_flow.py --fold '1' --trial 1 --model "mobilenetv3_large_100" --max_epoch 100 \
+--batch_size 256 --lr_scheduler "step_lr" --lr_scheduler_step 5 --lr_scheduler_factor 0.9 --cuda_list '1' \
+--IB_ratio 3 --random_seed 3829 --stage 'hem_train' --hem_extract_mode 'hem-emb-online' --inference_fold '1' \
+--use_wise_sample \
+--emb_type 2 --save_path '/code/OOB_Recog/logs/hem-online-3-1-step-ws-emb-half' > /dev/null &
+
+nohup python visual_flow.py --fold '1' --trial 1 --model "mobilenetv3_large_100" --max_epoch 100 \
+--batch_size 256 --lr_scheduler "step_lr" --lr_scheduler_step 5 --lr_scheduler_factor 0.9 --cuda_list '0' \
+--IB_ratio 3 --random_seed 3829 --stage 'hem_train' --hem_extract_mode 'hem-emb-online' --inference_fold '1' \
+--emb_type 2 --save_path '/code/OOB_Recog/logs/hem-online-3-1-step-emb-proxy-all' > /dev/null &
+
+
+
+
+
+nohup python visual_flow.py --fold '1' --trial 1 --model "mobilenetv3_large_100" --max_epoch 100 \
+--batch_size 256 --lr_scheduler "step_lr" --lr_scheduler_step 5 --lr_scheduler_factor 0.9 --cuda_list '2' \
+--IB_ratio 3 --random_seed 3829 --stage 'hem_train' --hem_extract_mode 'hem-emb-online' --inference_fold '1' \
+--use_online_mcd \
+--use_wise_sample \
+--emb_type 2 --save_path '/code/OOB_Recog/logs/hem-online-3-1-step-ws-emb-mcd' > /dev/null &
