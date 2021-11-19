e2068a096c68975d167610e3423018dd9a2597b8
hyeongyuc <hyeongyuc96@hutom.co.kr>
2021-11-20 01:58:44 +0900

    modify offline experiments scripts

--- template/scripts/mobilenet-offline-method.sh
+++ template/scripts/mobilenet-offline-method.sh
@@ -6,24 +6,24 @@ method_array=("hem-softmax-offline" "hem-voting-offline" "hem-vi-offline");
 
 for method in "${method_array[@]}";
 do
-    python visual_flow.py \
+    nohup python visual_flow.py \
         --fold "1" \
         --trial 1 \
         --model "mobilenetv3_large_100" \
         --pretrained \
         --use_lightning_style_save \
-        --max_epoch 1 \
+        --max_epoch 50 \
         --batch_size 256 \
         --lr_scheduler "step_lr" \
         --lr_scheduler_step 5 \
         --lr_scheduler_factor 0.9 \
-        --cuda_list "3" \
+        --cuda_list "1" \
         --random_seed 3829 \
         --IB_ratio 3 \
         --hem_extract_mode ${method} \
-        --top_ratio 10.0 \
+        --top_ratio 0.10 \
         --stage "hem_train" \
         --inference_fold "1" \
         --experiments_sheet_dir "/OOB_RECOG/results/offline-methods-experiment" \
-        --save_path "/OOB_RECOG/logs/offline-methods-experiment"
+        --save_path "/OOB_RECOG/logs/offline-methods-experiment" > /dev/null
 done;
--- template/scripts/mobilenet-offline-ratio.sh
+++ template/scripts/mobilenet-offline-ratio.sh
@@ -1,31 +1,29 @@
 # 21.11.19 off-line IB ratio별 실험
-# [고정] hem_extract_mode:hem-softmax-offline / random_seed:3829 / top_ratio:10.0
+# [고정] hem_extract_mode:hem-softmax-offline / random_seed:3829 / top_ratio:0.10
 # [변경] IB_ratio
 
 ratio_array=(3 4 5 7);
 
 for ratio in "${ratio_array[@]}";
 do
-    python visual_flow.py \
+    nohup python visual_flow.py \
         --fold "1" \
         --trial 1 \
         --model "mobilenetv3_large_100" \
         --pretrained \
         --use_lightning_style_save \
-        --max_epoch 1 \
+        --max_epoch 50 \
         --batch_size 256 \
         --lr_scheduler "step_lr" \
         --lr_scheduler_step 5 \
         --lr_scheduler_factor 0.9 \
-        --cuda_list "5" \
+        --cuda_list "2" \
         --random_seed 3829 \
         --IB_ratio ${ratio} \
         --hem_extract_mode "hem-softmax-offline" \
-        --top_ratio 10.0 \
+        --top_ratio 0.10 \
         --stage "hem_train" \
         --inference_fold "1" \
         --experiments_sheet_dir "/OOB_RECOG/results/offline-ratio-experiment" \
-        --save_path "/OOB_RECOG/logs/offline-ratio-experiment"
+        --save_path "/OOB_RECOG/logs/offline-ratio-experiment" > /dev/null
 done;
-
-# 1,2,3,5,7,10,18
