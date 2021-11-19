6cbf4fdb96bb3ac6fa19f52f3298300cf561a459
hyeongyuc <hyeongyuc96@hutom.co.kr>
2021-11-19 23:32:25 +0900

    rename offline experiments scripts and write new scripts for offline

---
+++ template/scripts/mobilenet-offline-method.sh
@@ -0,0 +1,29 @@
+# 21.11.19 off-line method별 실험
+# [고정] IB_ratio:4 / random_seed:3829 / top_ratio:10.0
+# [변경] hem_extract_mode
+
+method_array=("hem-softmax-offline" "hem-voting-offline" "hem-vi-offline");
+
+for method in "${method_array[@]}";
+do
+    python visual_flow.py \
+        --fold "1" \
+        --trial 1 \
+        --model "mobilenetv3_large_100" \
+        --pretrained \
+        --use_lightning_style_save \
+        --max_epoch 1 \
+        --batch_size 256 \
+        --lr_scheduler "step_lr" \
+        --lr_scheduler_step 5 \
+        --lr_scheduler_factor 0.9 \
+        --cuda_list "3" \
+        --random_seed 3829 \
+        --IB_ratio 3 \
+        --hem_extract_mode ${method} \
+        --top_ratio 10.0 \
+        --stage "hem_train" \
+        --inference_fold "1" \
+        --experiments_sheet_dir "/OOB_RECOG/results/offline-methods-experiment" \
+        --save_path "/OOB_RECOG/logs/offline-methods-experiment"
+done;
--- template/scripts/mobilenet-offline-ratio.sh
+++ template/scripts/mobilenet-offline-ratio.sh
@@ -1,4 +1,8 @@
-ratio_array=(1 3 5 7 10 18)
+# 21.11.19 off-line IB ratio별 실험
+# [고정] hem_extract_mode:hem-softmax-offline / random_seed:3829 / top_ratio:10.0
+# [변경] IB_ratio
+
+ratio_array=(3 4 5 7);
 
 for ratio in "${ratio_array[@]}";
 do
@@ -8,19 +12,20 @@ do
         --model "mobilenetv3_large_100" \
         --pretrained \
         --use_lightning_style_save \
-        --max_epoch 50 \
+        --max_epoch 1 \
         --batch_size 256 \
         --lr_scheduler "step_lr" \
         --lr_scheduler_step 5 \
         --lr_scheduler_factor 0.9 \
-        --cuda_list "1" \
+        --cuda_list "5" \
         --random_seed 3829 \
         --IB_ratio ${ratio} \
         --hem_extract_mode "hem-softmax-offline" \
+        --top_ratio 10.0 \
         --stage "hem_train" \
         --inference_fold "1" \
-        --experiments_sheet_dir "/OOB_RECOG/results/mobilenet-hem-offline-softmax-ratio" \
-        --save_path "/OOB_RECOG/logs/mobilenet-softmax-ratio_${ratio}"
-done
+        --experiments_sheet_dir "/OOB_RECOG/results/offline-ratio-experiment" \
+        --save_path "/OOB_RECOG/logs/offline-ratio-experiment"
+done;
 
 # 1,2,3,5,7,10,18
