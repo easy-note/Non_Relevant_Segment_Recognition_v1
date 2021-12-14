fc26ce3da62bd335cac839194b1e9a5264ff3ca4
hyeongyuc <hyeongyuc96@hutom.co.kr>
2021-12-14 11:25:54 +0900

    predict results per methods [figure visualization]

--- template/core/api/visualization.py
+++ template/core/api/visualization.py
@@ -23,10 +23,20 @@ class VisualTool:
         self.save_path = save_path # "~.png"
 
     def _get_plt(self, visual_type):
-        assert visual_type in ['predict', 'sampling'], 'NOT SUPPORT VISUAL MODE'
+        assert visual_type in ['predict', 'predict_multi', 'sampling'], 'NOT SUPPORT VISUAL MODE'
 
         if visual_type == 'predict':
             fig, ax = plt.subplots(3,1,figsize=(18,15)) # 1x1 figure matrix 생성, figsize=(가로, 세로) 크기지정
+
+            plt.subplots_adjust(left=0.125,
+                    bottom=0.1, 
+                    right=0.9, 
+                    top=0.9, 
+                    wspace=0, 
+                    hspace=0.35)
+
+        if visual_type == 'predict_multi':
+            fig, ax = plt.subplots(1,1,figsize=(30,7)) # 1x1 figure matrix 생성, figsize=(가로, 세로) 크기지정
 
             plt.subplots_adjust(left=0.125,
                     bottom=0.1, 
@@ -224,6 +234,183 @@ class VisualTool:
 
         ### file write
 	    # fig.tight_layout() # subbplot 간격 줄이기
+        os.makedirs(os.path.dirname(self.save_path), exist_ok=True)
+
+        plt.show()
+        plt.savefig(self.save_path, format='png', dpi=500)
+
+        plt.close(fig)
+    
+
+    def visual_predict_multi(self, predict_list_dict, model_name, inference_interval, window_size = 300, section_num = 2):
+        # TO-DO: if call gt_list from self.patient_name, better
+        # TO-DO: visualization oob ratio, metrics on ax
+        '''
+        predict_list: [1,0,0...]
+        model_name: str - 'mobilenet .. ' => title name
+        inferene_interval: int
+        inference_interval: number of elements in each section => title name
+        section number: number of elements in each section => title name
+        '''
+    
+        
+        fig, ax = self._get_plt('predict_multi')
+        
+        ### for plt variable, it should be pair synk
+        label_names = ['RS', 'NRS', 'FN', 'FP']
+        colors = ['cadetblue', 'orange', 'blue', 'red']
+        colors = ['navy', 'rosybrown', 'navy', 'rosybrown']
+        height = 0.7 # bar chart thic
+
+        ### prepare data ###
+        '''
+        visual_data = {'GT': [1, 1, 1, 0, 0, 1, 1, 1], # 0번 ~ 2번 frame , 5번 ~ 7번 frame
+	        'Model A': [0, 0, 1, 1, 0, 0, 1, 1], # 2번 ~ 3번 frame
+			'Model B': [1, 0, 1, 1, 1, 0, 1, 0] # 0번 ~ 0번 frame, 2번 ~ 4번 frame, 6번 ~ 6번 frame
+        }
+
+	    runlength_df = df([[3,0,0,1],
+				[2,0,0,0],
+				[3,0,0,1],
+				[0,2,0,0],
+				[0,2,0,1],
+				[0,2,0,0],
+				[0,2,0,1],
+				[0,0,1,1],
+				[0,0,1,0],
+				[0,0,3,1],
+				[0,0,1,0],
+				[0,0,1,1],
+				[0,0,1,0]], columns= yticks + ['class'])
+	    '''
+        visual_data = {}
+
+        for k,v in predict_list_dict.items():
+            visual_data['GT'], visual_data[k] = self.visual_helper.fit_to_min_length(self.gt_list, v)
+
+        gt_list = visual_data['GT']
+
+        frame_label = list(range(0, len(gt_list) * inference_interval, inference_interval))
+        time_label = [self.visual_helper.idx_to_time(idx, fps=30) for idx in frame_label]
+        yticks = ['GT', 'PREDICT'] # y축 names, 순서중요
+        yticks = ['GT'] + list(predict_list_dict.keys())
+
+        # change class for visual FP FN
+        for k, v in visual_data.items():
+            metrics = self.visual_helper.calc_metrics_with_index(gt_list, v)
+            # metrics_per_section = self.visual_helper.calc_section_metrics(gt_list, v, window_size, section_num)
+
+            # for visualize FP, FN // change class
+            predict_df = pd.DataFrame(v)
+            predict_df.iloc[metrics['FN_idx'],] = self.FN_CLASS
+            predict_df.iloc[metrics['FP_idx'],] = self.FP_CLASS
+
+            visual_data[k] = list(np.array(predict_df[0].tolist()))
+
+        # encoding data for visualization
+        encode_data = {}
+        for y_name in yticks : # run_length coding
+            encode_data[y_name] = pd.DataFrame(data=self.visual_helper.encode_list(visual_data[y_name]), columns=[y_name, 'class']) # [length, value]
+
+        # arrange data
+        runlength_df = pd.DataFrame(range(0,0)) # empty df
+        for y_name in yticks :
+            runlength_df = runlength_df.append(encode_data[y_name])
+
+        # Nan -> 0, convert to int
+        runlength_df = runlength_df.fillna(0).astype(int)
+
+        # split data, class // both should be same length
+        runlength_class = runlength_df['class'] # class info
+        runlength_model = runlength_df[yticks] # run length info of model prediction
+
+        ### draw ###
+        ##### initalize label for legned, this code should be write before writing barchart #####
+        init_bar = ax.barh(range(len(yticks)), np.zeros(len(yticks)), label=label_names[self.RS_CLASS], height=height, color=colors[self.RS_CLASS]) # dummy data
+        init_bar = ax.barh(range(len(yticks)), np.zeros(len(yticks)), label=label_names[self.NRS_CLASS], height=height, color=colors[self.NRS_CLASS]) # dummy data
+        init_bar = ax.barh(range(len(yticks)), np.zeros(len(yticks)), label=label_names[self.FN_CLASS], height=height, color=colors[self.FN_CLASS]) # dummy data
+        init_bar = ax.barh(range(len(yticks)), np.zeros(len(yticks)), label=label_names[self.FP_CLASS], height=height, color=colors[self.FP_CLASS]) # dummy data
+	    ##### #### #### #### ##### #### #### #### 
+
+        # data processing for barchart
+        data = np.array(runlength_model.to_numpy()) # width
+        data_cum = data.cumsum(axis=0) # for calc start index
+
+        # draw bar
+        for i, frame_class in enumerate(runlength_class) :
+            # print(data[i,:])
+            # print(frame_class)
+
+            widths = data[i,:]
+            starts= data_cum[i,:] - widths
+            
+            bar = ax.barh(range(len(yticks)), widths, left=starts, height=height, color=colors[frame_class]) # don't input label
+
+        # write text on PREDICT bar
+        '''
+        gt_oob_ratio = metrics['gt_OOB'] / (metrics['gt_IB'] + metrics['gt_OOB'])
+        predict_oob_ratio = metrics['predict_OOB'] / (metrics['predict_IB'] + metrics['predict_OOB'])
+        text_bar = ax.barh(1, 0, height=height) # dummy data        
+        self.present_text(ax, text_bar, 'CR : {:.3f} | OR : {:.3f} | JACCARD: {:.3f} | OOB_RATIO(GT) : {:.2f} | OOB_RATIO(PD) : {:.2f}'.format(metrics['CR'], metrics['OR'], metrics['Jaccard'], gt_oob_ratio, predict_oob_ratio))
+        '''
+
+        ### write on figure 
+        # set title
+        title_name = 'Title will be removed [Predict of {}]'.format(self.patient_name)
+        # sub_title_name = 'model: {} | inferene interval: {} | windows size: {} | section num: {}'.format(model_name, inference_interval, window_size, section_num)
+        sub_title_name =  'Model {}'.format(model_name)
+        fig.suptitle(title_name, fontsize=20)
+        # ax.set_title(sub_title_name, fontsize=16)
+
+        # non visual
+        ax.xaxis.set_visible(False) # non visual x label
+        ax.spines['left'].set_visible(False) # ax non visual boundary
+        ax.spines['right'].set_visible(False)
+        ax.spines['top'].set_visible(False)
+        ax.spines['bottom'].set_visible(False)
+
+        # set xticks pre section size
+        ax.set_xticks(range(0, len(frame_label), window_size))
+        
+        xtick_labels = ['{}\n{}'.format(time, frame) if i_th % 2 == 0 else '\n\n{}\n{}'.format(time, frame) for i_th, (time, frame) in enumerate(zip(frame_label[::window_size], time_label[::window_size]))]
+        ax.set_xticklabels(xtick_labels) # xtick change
+        ax.xaxis.set_tick_params(labelsize=6)
+        ax.set_xlabel('Frame / Time (h:m:s:fps)', fontsize=12)
+
+
+        # set yticks
+        ax.set_yticks(range(len(yticks)))
+        ax.set_yticklabels(yticks, fontsize=20)
+        for label in ax.get_yticklabels(): label.set_fontweight('bold')
+        # ax.set_ylabel('Methods', fontsize=20)
+
+        # 8. legend
+        box = ax.get_position() # 범례를 그래프상자 밖에 그리기위해 상자크기를 조절
+        ax.set_position([box.x0, box.y0, box.width * 0.9, box.height])
+        # ax.legend(label_names, loc='center left', bbox_to_anchor=(1,0.5), shadow=True, ncol=1)
+
+        # 9. 보조선(눈금선) 나타내기
+        '''
+        ax.set_axisbelow(True)
+        ax.xaxis.grid(True, color='gray', linestyle='dashed', linewidth=0.5)
+        '''
+
+        # 10. draw subplot ax (section metrics)
+        '''
+        section_start_idx = np.array(metrics_per_section['start_idx']) * inference_interval
+        section_start_idx = section_start_idx.tolist()
+        CR_value = metrics_per_section['section_CR']
+        OR_value = metrics_per_section['section_OR']
+
+        CR_value = [1.0 if val==self.EXCEPTION_NUM else val for val in CR_value] # -100 EXP 일 경우 1로 처리
+        OR_value = [0.0 if val==self.EXCEPTION_NUM else val for val in OR_value] # -100 EXP 일 경우 0로 처리
+        
+        self.draw_plot(ax[1], 'CR of Predict', section_start_idx, CR_value, y_min=-1.0, y_max=1.0, color='blue')
+        self.draw_plot(ax[2], 'OR of Predict', section_start_idx, OR_value, y_min=0.0, y_max=1.0, color='red')
+        '''
+
+        ### file write
+        fig.tight_layout() # subbplot 간격 줄이기
         os.makedirs(os.path.dirname(self.save_path), exist_ok=True)
 
         plt.show()
---
+++ template/scripts/visual_figure.py
@@ -0,0 +1,85 @@
+def visual_for_prediction_results(patient_gt_list, patient_no, model_name, results_dict, save_dir):
+    from core.api.visualization import VisualTool # visual module
+
+    import os
+    import pandas as pd
+    import glob
+
+    os.makedirs(save_dir, exist_ok=True)
+
+    save_path = os.path.join(save_dir, 'prediction_results_of_{}.png'.format(patient_no))
+
+    visual_tool = VisualTool(patient_gt_list, patient_no, save_path)
+    visual_tool.visual_predict_multi(results_dict, model_name, inference_interval=30, window_size=300, section_num=2)
+
+
+def get_inference_results_per_patient(inference_results_dir, patient_no):
+    import os
+
+    from core.utils.parser import FileLoader
+    from core.utils.prepare import PatientsGT
+
+    file_loader = FileLoader()    
+
+    patient_gt = PatientsGT()
+    videos = patient_gt.get_video_no(patient_no)
+
+    patient_predict_results_dir = os.path.join(inference_results_dir, patient_no)
+
+    patient_predict_list = []
+
+    for video_no in videos:
+        video_predict_results_path = os.path.join(patient_predict_results_dir, video_no, '{}.csv'.format(video_no))
+        
+        file_loader.set_file_path(video_predict_results_path)
+        predict_df = file_loader.load()
+
+        patient_predict_list += predict_df['predict'].tolist()
+
+    return patient_predict_list
+
+if __name__ == '__main__':
+    if __package__ is None:
+        import sys
+        import os
+        from os import path    
+        base_path = path.dirname(path.dirname(path.abspath(__file__)))
+        sys.path.append(base_path)
+        sys.path.append(base_path+'/core/accessory/RepVGG')
+        print(base_path)
+        
+        from core.utils.misc import save_dict_to_csv, prepare_inference_aseets, get_inference_model_path, \
+                set_args_per_stage, check_hem_online_mode, clean_paging_chache
+
+        from core.utils.parser import FileLoader
+        from core.utils.prepare import PatientsGT
+
+    patient_no = 'R_2'
+    model_name = 'mobilenet_v3_large'
+    save_dir = '/OOB_RECOG/figures/predictions'
+
+    patient_gt = PatientsGT()
+    patient_gt_list = patient_gt.get_gt(patient_no)
+    patient_gt_list = patient_gt_list[::30]
+
+    results_dict = {
+        '(a)': get_inference_results_per_patient(os.path.join('/OOB_RECOG', 'logs', 'mobilenet_apply_offline_methods-all-offline-IB_ratio=3-ws_ratio=3-MC=5-experiment-model:mobilenetv3_large_100-IB_ratio:3.0-WS_ratio:3-hem_extract_mode:all-offline-top_ratio:0.07-seed:3829', 'TB_log', 'version_4', 'inference_results'), patient_no),
+        '(b)': get_inference_results_per_patient(os.path.join('/OOB_RECOG', 'logs', 'mobilenet_apply_offline_methods-all-offline-IB_ratio=3-ws_ratio=3-MC=5-experiment-model:mobilenetv3_large_100-IB_ratio:3.0-WS_ratio:3-hem_extract_mode:all-offline-top_ratio:0.07-seed:3829', 'TB_log', 'version_5', 'inference_results'), patient_no),
+        '(c)': get_inference_results_per_patient(os.path.join('/OOB_RECOG', 'logs', 'mobilenet_apply_offline_methods-all-offline-IB_ratio=3-ws_ratio=3-MC=5-experiment-model:mobilenetv3_large_100-IB_ratio:3.0-WS_ratio:3-hem_extract_mode:all-offline-top_ratio:0.07-seed:3829', 'TB_log', 'version_6', 'inference_results'), patient_no),
+        '(d)': get_inference_results_per_patient(os.path.join('/OOB_RECOG', 'logs', 'mobilenet_apply_offline_methods-all-offline-IB_ratio=3-ws_ratio=3-MC=5-experiment-model:mobilenetv3_large_100-IB_ratio:3.0-WS_ratio:3-hem_extract_mode:all-offline-top_ratio:0.07-seed:3829', 'TB_log', 'version_7', 'inference_results'), patient_no),
+        '(e)': get_inference_results_per_patient(os.path.join('/OOB_RECOG', 'logs', 'mobilenet_apply_offline_methods-all-offline-IB_ratio=3-ws_ratio=3-MC=5-experiment-model:mobilenetv3_large_100-IB_ratio:3.0-WS_ratio:3-hem_extract_mode:all-offline-top_ratio:0.07-seed:3829', 'TB_log', 'version_7', 'inference_results'), patient_no),
+        '(f)': get_inference_results_per_patient(os.path.join('/OOB_RECOG', 'logs', 'mobilenet_apply_offline_methods-all-offline-IB_ratio=3-ws_ratio=3-MC=5-experiment-model:mobilenetv3_large_100-IB_ratio:3.0-WS_ratio:3-hem_extract_mode:all-offline-top_ratio:0.07-seed:3829', 'TB_log', 'version_7', 'inference_results'), patient_no),
+    }
+
+    visual_for_prediction_results(patient_gt_list, patient_no, model_name, results_dict, save_dir) # figure b, predict results per methods
+
+
+
+    
+
+
+
+
+
+
+
