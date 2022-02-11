70329569b03d354932f3bd00a383e8fb2e6dd65c
jihyun98hutom <jihyun.lee@hutom.io>
2022-02-11 13:39:58 +0900

    Only visualization proceeds

---
+++ template/scripts/jhlee/new_visualization.py
@@ -0,0 +1,33 @@
+
+
+
+import sys
+from os import path    
+base_path = path.dirname(path.dirname(path.dirname(path.abspath(__file__))))
+print(base_path)
+sys.path.append(base_path)
+
+import os
+import glob
+import pandas as pd
+from core.api.visualization import VisualTool # visual module
+
+
+base_path = '/OOB_RECOG/logs-new'
+model = 'mobilenet' # ['mobilevit', 'resnet']
+
+target_path  = glob.glob(os.path.join(base_path, model+'*','hem_assets' ,'*.csv')) 
+
+for i in target_path:
+    print(i)
+    
+    hem_assets_path = i
+    hem_df = pd.read_csv(hem_assets_path)   
+
+    method_info = '-'.join(i.split('/')[-1].split('.')[0].split('-')[:-1])
+
+    save_dir = os.path.join('/'.join(hem_assets_path.split('/')[:-1]), method_info)
+
+    print(save_dir)
+
+    test_visual_sampling.visual_flow_for_sampling(hem_df, model, save_dir)
